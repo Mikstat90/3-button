a3b3c0bbd2ae773eb48fe6e87fdfe23adfde1eac
Dzmitry Tsitarau <mikstat90@gmail.com>
2023-10-02 23:31:53 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {MouseEvent} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Hello Im Dima')
+    // }
+    //
+    // const mySecondSubscriber = () => {
+    //     console.log('Hello Im Mira')
+    // }
+
+    const onClickHandler = (name: string) => {
+        console.log(name)
+    }
+
+    // const foo1 = () => {
+    //     console.log(100200)
+    // }
+    // const foo2 = (num: number) => {
+    //     console.log(num)
+    // }
+
+    return (
+        <div className="App">
+            {/*<button onClick={(event)=>{console.log('Hello')}}>My YouTubeChanel-1</button>*/}
+
+            {/*<button onClick={myFirstSubscriber}>My YouTubeChanel-1</button>*/}
+            {/*<button onClick={mySecondSubscriber}>My YouTubeChanel-2</button>*/}
+
+            <button onClick={(event: MouseEvent<HTMLButtonElement>)=>onClickHandler('Dima')}>My YouTubeChanel-1</button>
+            <button onClick={(event: MouseEvent<HTMLButtonElement>)=>onClickHandler('Mira')}>My YouTubeChanel-2</button>
+            {/*<button onClick={()=>onClickHandler('some info')}>My YouTubeChanel-3</button>*/}
+
+            {/*<button onClick={foo1}>1</button>*/}
+            {/*<button onClick={()=>foo2(100200)}>2</button>*/}
+        </div>
+    );
 }
 
 export default App;
